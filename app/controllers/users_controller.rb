aa9295ab913bfc62a7f5966c26cebd005894b75c
Bilal9111 <mbc000@live.com>
2017-08-02 14:56:23 +0300

    staring chap12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index, :delete]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-
+  before_action :is_activated?,  only: :show
 
 
 
@@ -16,18 +16,18 @@ class UsersController < ApplicationController
   	#debugger
   end
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      log_in @user
-    	flash[:success] = "Welcome to the Sample App!"
-    	redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def edit
@@ -52,6 +52,17 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   private
+    #This method helps to make sure that only the activated users can be SHOWN
+    def is_activated?
+      @user = params[:activated?]
+      if @user
+        #if is activated then dont do anything
+      else
+        flash[:success] = "The user requested is not activated.If this is your account, then please check your email. :)"
+        redirect_to root_url
+       end
+    end
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
